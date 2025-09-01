d4136e6f0e06751d3c58fd58c5b44cc9eba2f980
Andrew Macmillan <andrew.macmillan@ymail.com>
2025-09-01 14:46:51 +0100

    update: implement mobile nav

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
 (function () {
     console.log("JavaScript is running!");
+
+    const burgerIcon = document.getElementById("burger-icon");
+    const closeIcon = document.getElementById("close-icon");
+    const mobileNav = document.querySelector(".mobile-nav-container");
+
+    burgerIcon.addEventListener("click", function () {
+        closeIcon.style.display = "block";
+        mobileNav.style.display = "flex";
+    });
+
+    closeIcon.addEventListener("click", function () {
+        closeIcon.style.display = "none";
+        mobileNav.style.display = "none";
+    });
 })();
