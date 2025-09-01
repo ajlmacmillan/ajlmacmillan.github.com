673ecd71efb9841b855176b25049c3e0c945bcd2
Andrew Macmillan <andrew.macmillan@ymail.com>
2025-09-01 22:26:14 +0100

    update: add data JSON files for card population

--- js/index.js
+++ js/index.js
@@ -14,4 +14,33 @@
         closeIcon.style.display = "none";
         mobileNav.style.display = "none";
     });
+
+    window.addEventListener("load", () => {
+        // get the path to determine JSON file
+        const path = window.location.href;
+
+        // mapping of paths to JSON files
+        const fileMap = {
+            "index.html": "announcements.json",
+            "pages/blog/main.html": "blog.json",
+            "pages/creative/main.html": "creative.json",
+            "pages/portfolio/main.html": "portfolio.json",
+        };
+
+        // get JSON data file name based on path
+        let dataFile = "announcements.json"; // default file
+        for (const [key, value] of Object.entries(fileMap)) {
+            if (path.includes(key)) {
+                dataFile = value;
+                break;
+            }
+        }
+
+        // fetch JSON data
+        fetch(`https://www.livingcopy.uk/data/${dataFile}`)
+            .then((response) => response.json())
+            .then((json) => {
+                console.log(json);
+            });
+    });
 })();
